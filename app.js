022dabf614a253ede98c3ec029315f01bc7b6b12
zoomviex <agvazvinicius@gmail.com>
2023-03-23 15:51:29 -0300

    break 03:10:31

---
+++ app.js
@@ -0,0 +1,41 @@
+const timeLeftDisplya = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const StarPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+console.log(squares);
+
+let currentIndex = 76;
+const width = 9;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentIndex % width !== 0) {
+        currentIndex -= 1;
+      }
+      break;
+    case "ArrowRight":
+      if (currentIndex % width < width - 1) {
+        currentIndex += 1;
+      }
+      break;
+    case "ArrowUp":
+      if (currentIndex - width >= 0) {
+        currentIndex -= width;
+      }
+      break;
+    case "ArrowDown":
+      if (currentIndex + width < width * width) {
+        currentIndex += width;
+      }
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
+}
+document.addEventListener("keyup", moveFrog);
+
+function moveLogLeft(){
+
+}
